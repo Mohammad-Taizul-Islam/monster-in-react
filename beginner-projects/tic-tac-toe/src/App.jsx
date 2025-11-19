78a8626774b17925d9a0fe2dca4c86d24d797d08
Mohammad-Taizul-Islam <m.taizul1045@gmail.com>
2025-11-19 09:01:55 +0600

    tic-tac-toe v2

--- beginner-projects/tic-tac-toe/src/App.jsx
+++ beginner-projects/tic-tac-toe/src/App.jsx
@@ -1,6 +1,9 @@
-const Square = ({ value }) => {
+import { useState } from "react";
+
+const Square = () => {
+  const [value, setValue] = useState(null);
   const handleClick = () => {
-    console.log("clicked!");
+    setValue("X");
   };
   return (
     <button className="square" onClick={handleClick}>
@@ -12,19 +15,19 @@ const Board = () => {
   return (
     <>
       <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
